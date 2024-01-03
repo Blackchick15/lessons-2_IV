fa5fdccc8e8419cd4b5e9ef763c524fc547ec1c5
Blackchick15 <109077737+Blackchick15@users.noreply.github.com>
2024-01-03 20:47:48 +0200

    commit

--- js/04-scroll.js
+++ js/04-scroll.js
@@ -1,1 +1,24 @@
+const header = document.querySelector('.header');
+const nav = document.querySelector('.nav');
+pageYOffset;
+
+let headerHeight = header.clientHeight;
+
+function navbarFixet(){
+if(pageYOffset > headerHeight ){
+    nav.classList.add('fixed-nav');
+}else{
+    nav.classList.remove('fixed-nav');
+}
+}
+
+window.addEventListener('scroll', navbarFixet);
+
+
+
+
+console.log(header.clientHeight);
+
+// console.log(header.clientWidth);
+
 
