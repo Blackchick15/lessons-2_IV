58d90f9af08925b2fefaf2a423bf2db66fb71e96
Blackchick15 <109077737+Blackchick15@users.noreply.github.com>
2024-01-10 19:32:29 +0200

    commit

--- js/05-observer.js
+++ js/05-observer.js
@@ -1,23 +1,38 @@
-const targetElement = document.querySelector('.animated-text');
+// const targetElement = document.querySelector('.animated-text');
 
-const handleObserver = entries => {
+// const handleObserver = entries => {
 
-  console.log('entris', entries);
+//   console.log('entris', entries);
 
-  entries.forEach(entry => {
-    if(entry.isIntersecting){
-        entry.target.classList.add('visible');
-    } else{
-        entry.target.classList.remove('visible');
-    };
-  })
+//   entries.forEach(entry => {
+//     if(entry.isIntersecting){
+//         entry.target.classList.add('visible');
+//     } else{
+//         entry.target.classList.remove('visible');
+//     };
+//   })
 
-};
-
-
-
-const wotcher = new IntersectionObserver(handleObserver, { threshold: 1 });
-
-wotcher.observe(targetElement);
+// };
 
 
+
+// const wotcher = new IntersectionObserver(handleObserver, { threshold: 1 });
+
+// wotcher.observe(targetElement);
+
+
+const videoPlayer = document.querySelector('video');
+
+const hendelVideoObserve = () =>{
+  if(!videoPlayer.paused){
+    videoPlayer.pause();
+  }else{
+    videoPlayer.play();
+  }
+}
+
+const watcher = new IntersectionObserver(hendelVideoObserve, {
+  threshold: 0.5,
+});
+
+watcher.observe(videoPlayer);
